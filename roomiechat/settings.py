72a07c56451b8ecd2cffc4c22c8ca0ba6426cc4c
tcfabritius <tim.fabritius@skiff.com>
2023-03-13 10:42:56 +0200

    vercel

--- roomiechat/settings.py
+++ roomiechat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ys_d_50=*#7ukkm3(cuku_bayqi)d=(u7oqc#(tqj4u7xcy6rr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
